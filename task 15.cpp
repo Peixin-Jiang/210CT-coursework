ea34d9916c0a3ba3330c1c6759023db8dee38938
Peixin-Jiang <171316396@qq.com>
2016-11-30 22:35:06 +0800

    Add files via upload

---
+++ task 15.cpp
@@ -0,0 +1,95 @@
+#include<iostream>
+using namespace std;
+class Vertex
+{
+public:
+	int value,k,tw;
+	Vertex* AdjacentList[10];
+	int weight[10];
+	Vertex* pre; 
+	Vertex(int value)
+	{
+		this->value=value;
+		this->k = 0;
+		this->tw=1000;
+	}
+	void AddEdge(Vertex* v,int w)
+	{
+		AdjacentList[k]=v;
+		k++;
+	}
+};
+
+void dijkstra(Vertex** graph,Vertex* s,Vertex* d)
+{
+	Vertex* v=s;
+    s->tw=0;
+    Vertex** visited=new Vertex*[10];
+    	int t=0;
+    bool condition=false;
+    while(v!=d)
+    {
+    	//cout<<"WHILE"<<endl;
+    	for(int i=0;i<v->k;i++)
+    	{
+    		if((v->tw+v->weight[i])<(v->AdjacentList[i]->tw))
+    		{
+    			v->AdjacentList[i]->tw=v->tw+v->weight[i];
+    			v->AdjacentList[i]->pre=v;
+        		cout<<"GRAPH tw=["<<graph[0]->tw<<"|"<<graph[1]->tw<<"|"<<graph[2]->tw<<"|"<<graph[3]->tw<<"|"<<graph[4]<<"]"<<endl; 
+    		}
+    	}
+    	break; // temp work
+        visited[t]=v;
+        t++;
+      int	min=1000;
+    	for(int i=0;i<5;i++)
+		{
+			condition=false;
+			for(int j=0;j<t;j++)
+    	   {
+    	   		//cout<<"CONDITION: j="<<j<<" t="<<t<<" visited="<<visited[j]->value<<" graph="<<graph[i]->value<<endl;
+		 
+	            if(visited[j]->value==graph[i]->value)
+		       {
+		           	condition=true;
+			        break;
+	        	}
+        	}
+        	cout<<"LOOP "<<!condition<<" && "<<graph[i]->tw << "<" << min << &endl;
+        	if(!condition&&graph[i]->tw<min)
+		    {
+	     		v=graph[i];
+	     		cout<<"IF"<<v->value<< &endl; 
+	    		min=v->tw;
+	    	}
+ 	    }
+    }
+}
+
+int main()
+{
+	Vertex* a=new Vertex(1);
+	Vertex* b=new Vertex(2);
+	Vertex* c=new Vertex(3);
+	Vertex* d=new Vertex(4);
+	Vertex* e=new Vertex(5);
+	Vertex** graph=new Vertex*[5];
+	graph[0] = a;
+	graph[1] = b;
+	graph[2] = c;
+	graph[3] = d;
+	graph[4] = e;
+	
+	
+	a->AddEdge(b,1);
+	a->AddEdge(e,10);
+	b->AddEdge(c,5);
+	c->AddEdge(e,1);
+	d->AddEdge(c,2);
+	d->AddEdge(e,6);
+	a->AddEdge(d,3);
+	dijkstra(graph,a,e);
+	cout<<e->tw;
+}
+
